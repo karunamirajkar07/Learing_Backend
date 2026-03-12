04f83d7e3b34e2e0cd5f2a114a77885813dc7e60
karuna mirajkar <kmirajkar@hexalytics.com>
2026-03-12 11:59:21 +0530

    update

--- Archive/index.js
+++ Archive/index.js
@@ -12,5 +12,5 @@ app.use(express.json())
 app.use("/api", userRouter)
 
 app.listen(process.env.PORT, ()=>{
- console.log("Server running on port", 5000)
+ console.log("Server running on port", process.env.PORT || 5000)
 })
